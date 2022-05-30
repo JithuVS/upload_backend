94a4434cb2b5bfe01e61cf5517382f257de20d45
JithuVS <sreejithsalgunan@gmail.com>
2022-05-30 10:00:05 +0530

    cors

--- index.js
+++ index.js
@@ -18,30 +18,35 @@ var port = process.env.PORT;
 //     credentials: true,
 //   })
 // );
-//app.use(cors({ credentials: true, origin: "https://uplloads.herokuapp.com"}));
- app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://uplloads.herokuapp.com"
-  );
+app.use(
+  cors({
+    origin: "https://uplloads.herokuapp.com",
+    credentials: true,
+    methods: ["GET", "POST"],
+  })
+);
+//  app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://uplloads.herokuapp.com"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "POST, GET");
-  
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "POST, GET");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 app.use(express.json());
 app.use(bodyParser.json());
