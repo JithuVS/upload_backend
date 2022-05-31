3ca46b6b3c3e70f218ef95eb64eb4068eaa8f461
JithuVS <sreejithsalgunan@gmail.com>
2022-05-31 10:50:33 +0530

    folder path

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,7 +1,7 @@
 var multer = require("multer");
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, "./");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
